dec4c4ae3caf90530b46cfbd3fb959ff7fe63c20
bookRa <omar.abdelbadie1@gmail.com>
2022-06-21 07:56:46 -0700

    switch to evtv2 folder

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,12 @@
 namespace Config {
   export const TEMPLATE_SPREADSHEET_ID =
-    "1FhGgrfuZfiofKAEN86_6kpqP44sduN95JSiJApyWTPk";
+    "1tnCc71TWeCbLdSJKN80OHGn8CbrsCxp3Hh5O4Zd_Hh8";
   // TODO: when there are >1 Results sheet we can iterate and look at all of them
   export const RESULTS_SPREADSHEET_ID =
-    "10Lp-bNiU9Hg1yuyYjpnF7vYc0iNFhfgTo7CqfwbqISs";
-  export const EC_FOLDER_ID = "1PeEFnwc8tIP189L4cm2HDoBO6Zdwuj0w";
+    "10Lp-19hNFNqlVvFXjBLyOnxVtV8YIe7e2PMrdmVWvkdxqTgg";
+  export const EC_FOLDER_ID = "14NslVP5LOFWdsKZaVWLWLNjVg_XvmR-J";
   export const NUMBER_OF_EQUITY_FACTORS = 4; // TODO: hardcoded for now based on Template file
-  export const FULL_NAME_QUESTION = "Your Full Name and Pronouns:";
+  export const FULL_NAME_QUESTION = "your name";
   export const TEMPLATE_CATEGORIES_SHEET_NAME = "Reference";
   export const TEMPLATE_QUESTION_BANK_SHEET_NAME = "Question_Bank";
 }
